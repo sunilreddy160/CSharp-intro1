2722eb6b8fe1b4efae2e6400d4b26a8da5ead963
sunilreddy160 <92992426+sunilreddy160@users.noreply.github.com>
2021-11-16 20:13:00 +0530

    Update Program.cs

--- Csharp classes/Program.cs
+++ Csharp classes/Program.cs
@@ -6,7 +6,30 @@ namespace Csharp_classes
     {
         static void Main(string[] args)
         {
+             Console.WriteLine("Hello World!");
+            customer o = new customer("sunil");
+
+            Console.WriteLine(o.name);
+            customer.print();
+            customer.print1();
             Console.WriteLine("Hello World!");
+        }
+         public class customer
+        {
+            public string name;
+            public customer(string name)
+            {
+                this.name = name;
+
+            }
+            public static void print()
+            {
+                Console.WriteLine("hello world");
+            }
+            public static void print1()
+            {
+                Console.WriteLine("hello");
+            }
         }
     }
 }
